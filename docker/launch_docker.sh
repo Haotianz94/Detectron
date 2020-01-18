80e3344f1bace447ac827fb5c3a3ec2f8c7fe4f4
haotianz <haotianz@stanford.edu>
2020-01-17 20:19:20 -0800

    add script for update mask in racket2game

---
+++ docker/launch_docker.sh
@@ -0,0 +1,14 @@
+# Thanks @dustinfreeman for providing the script
+#!/bin/bash
+# nvidia-docker build -f docker/Dockerfile -t detect_and_track:CUDA9-py2 . 
+
+# run from ~/Projects
+# nvidia-docker run -ti --ipc=host --shm-size 12G -v $(pwd):/home/haotian/Projects/ --workdir=/home/haotian/Projects detect_and_track:CUDA9-py2 /bin/bash
+# nvidia-docker run -ti --ipc=host --shm-size 12G -v $(pwd):/Detectron --workdir=/Detectron detect_and_track:CUDA9-py2 /bin/bash
+
+docker login
+
+docker tag detect_and_track:CUDA9-py2 docker.io/haotianz/detect_and_track:CUDA9-py2
+
+docker push haotianz/detect_and_track:CUDA9-py2
+
